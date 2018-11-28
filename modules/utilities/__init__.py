e6a766fa9aedb96a4ee6aac731a4bdb4c4f49320
michael <michael_ball@brown.edu>
2018-11-28 15:59:32 -0500

    improving preprocessing/utility structure

--- modules/utilities/__init__.py
+++ modules/utilities/__init__.py
@@ -1,4 +1,4 @@
-from .dependencies import my_dependencies
+from .dependencies import all_dependencies, my_dependencies
 from .helpers import randomise, V
-from .preprocessing import tokenise
+from .preprocessing import create_vocab, tokenise, tokenise_and_embed
 from .visualise import plot_train_test_loss
--- modules/utilities/preprocessing.py
+++ modules/utilities/preprocessing.py
@@ -2,10 +2,19 @@ import gensim.downloader as api
 import numpy as np
 import spacy
 
+from collections import Counter, defaultdict
 from pathlib import Path
 
 
-def tokenise(we_source, data):
+class EmbeddingNode():
+    def __init__(self, we, node):
+        self.text = node.text.lower()
+        self.dep = node.dep_
+        self.embedding = we[self.text] if self.text in we else we['unk']
+        self.children = [EmbeddingNode(we, c) for c in node.children]
+
+
+def tokenise_and_embed(we_source, data):
     we = api.load(we_source)
     nlp = spacy.load('en')
     tokenised = []
@@ -19,9 +28,22 @@ def tokenise(we_source, data):
     return tokenised
 
 
-class EmbeddingNode():
-    def __init__(self, we, node):
-        self.text = node.text.lower()
-        self.dep = node.dep_
-        self.embedding = we[self.text] if self.text in we else we['unk']
-        self.children = [EmbeddingNode(we, c) for c in node.children]
+def tokenise(data):
+    nlp = spacy.load('en')
+    tokenised = []
+    for x in data:
+        s1 = [t.text.lower() for t in nlp(str(x[0]))]
+        s2 = [t.text.lower() for t in nlp(str(x[1]))]
+        score = float(x[2])
+        tokenised.append([s1, s2, score])
+    
+    return tokenised
+
+
+def create_vocab(sentences):
+    freq = Counter(p for o in sentences for p in o)
+    vocab = [o for o, c in freq.items() if c > 1] # could use >2?
+    vocab.insert(0, 'unk')
+    string2idx = defaultdict(lambda: 0, {v: k for k,v in enumerate(vocab)})
+
+    return vocab, string2idx
