bffc4276d40aea564da0f20bca38020ede5cae5c
michael <michael_ball@brown.edu>
2018-11-28 20:57:17 -0500

    added attribute for storing phrase representation

--- modules/utilities/preprocessing.py
+++ modules/utilities/preprocessing.py
@@ -10,6 +10,7 @@ class EmbeddingNode():
     def __init__(self, we, node):
         self.text = node.text.lower()
         self.dep = node.dep_
+        self.representation = None
         self.embedding = we[self.text] if self.text in we else we['unk']
         self.children = [EmbeddingNode(we, c) for c in node.children]
 
