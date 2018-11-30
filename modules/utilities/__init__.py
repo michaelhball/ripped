fab065f1dba3c90af13cfbffa6b2e403cf52e82f
michael <michael_ball@brown.edu>
2018-11-30 16:02:05 -0500

    ^^

--- modules/utilities/__init__.py
+++ modules/utilities/__init__.py
@@ -1,4 +1,5 @@
 from .dependencies import all_dependencies, my_dependencies
 from .helpers import randomise, V
 from .preprocessing import create_vocab, tokenise, tokenise_and_embed
+from .tree import EmbeddingNode
 from .visualise import plot_train_test_loss
