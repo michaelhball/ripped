14a118c6046c8fe1110c603dbe40a2795b3624a7
michael <michael_ball@brown.edu>
2018-11-30 16:01:20 -0500

    more extensible + redefined STS predictor

--- modules/models/__init__.py
+++ modules/models/__init__.py
@@ -1,6 +1,6 @@
 from .basic_classifier import BasicClassifier
 from .cosine_sim_sts_predictor import CosineSimSTSPredictor
-from .dependency_encoder import DependencyEncoder
+from .dependency_encoder_1 import DependencyEncoder1
 from .dependency_encoder_2 import DependencyEncoder2
 from .dependency_encoder_3 import DependencyEncoder3
 from .euclid_sim_sts_predictor import EuclidSimSTSPredictor
@@ -9,24 +9,23 @@ from .pooling_linear_classifier import PoolingLinearClassifier
 from .sts_predictor import STSPredictor
 
 
-def create_sts_predictor(nlp, word_embeddings, embedding_dim, batch_size, dependency_map, layers, use_bias=False, drops=None):
-    encoder = DependencyEncoder(nlp, word_embeddings, embedding_dim, batch_size, dependency_map, use_bias)
+dependency_encoders = {
+    "1": DependencyEncoder1,
+    "2": DependencyEncoder2,
+    "3": DependencyEncoder3
+}
 
+
+def create_sts_predictor(embedding_dim, batch_size, dependency_map, encoder_model, layers, drops, use_bias=False):
+    encoder = dependency_encoders[encoder_model](embedding_dim, batch_size, dependency_map)
     return STSPredictor(encoder, layers, drops)
 
 
-def create_cosine_sim_sts_predictor(embedding_dim, batch_size, dependency_map, encoder_model=1):
-    if encoder_model == 1:
-        encoder = DependencyEncoder(embedding_dim, batch_size, dependency_map)
-    elif encoder_model == 2:
-        encoder = DependencyEncoder2(embedding_dim, batch_size, dependency_map)
-    elif encoder_model == 3:
-        encoder = DependencyEncoder3(embedding_dim, batch_size, dependency_map)
-
+def create_cosine_sim_sts_predictor(embedding_dim, batch_size, dependency_map, encoder_model):
+    encoder = dependency_encoders[encoder_model](embedding_dim, batch_size, dependency_map)
     return CosineSimSTSPredictor(encoder)
 
 
-def create_euclid_sim_sts_predictor(embedding_dim, batch_size, dependency_map):
-    encoder = DependencyEncoder(embedding_dim, batch_size, dependency_map)
-
+def create_euclid_sim_sts_predictor(embedding_dim, batch_size, dependency_map, encoder_model):
+    encoder = dependency_encoders[encoder_model](embedding_dim, batch_size, dependency_map)
     return EuclidSimSTSPredictor(encoder)
--- modules/models/sts_predictor.py
+++ modules/models/sts_predictor.py
@@ -9,48 +9,20 @@ class STSPredictor(nn.Module):
     """
     A class to predict similarity between two input sentences.
     """
-    def __init__(self, encoder, layers, drops=None):
+    def __init__(self, encoder, layers, drops):
         super().__init__()
         self.encoder = encoder
-        if drops:
-            self.layers = nn.ModuleList([LinearBlock(layers[i], layers[i+1], drops[i]) for i in range(len(layers) - 1)])
-        else:
-            self.layers = nn.ModuleList([nn.Linear(layers[i], layers[i+1]) for i in range(len(layers)-1)])
+        self.layers = nn.ModuleList([LinearBlock(layers[i], layers[i+1], drops[i]) for i in range(len(layers) - 1)])
     
-    def forward(self, input1, input2): # this isn't set up for batching yet
+    def forward(self, input1, input2):
         x_1 = self.encoder(input1) # 1xd
         x_2 = self.encoder(input2) # 1xd
-        diff = (x_1-x_2).abs() # 1xd
-        mult = x_1 * x_2 # 1xd
-
-        x = torch.cat((x_1, x_2, diff, mult), 0).reshape(1,-1) # 1x4d
+        # diff = (x_1-x_2).abs() # 1xd
+        # mult = x_1 * x_2 # 1xd
+        x = torch.cat((x_1, x_2), 0).reshape(1,-1) # 1x2d
+        
         for l in self.layers:
             l_x = l(x)
             x = F.relu(l_x)
 
-        return l_x#, x # 1x1
-
-
-# if __name__ == "__main__":
-#     import gensim.downloader as api
-#     import spacy
-#     from dependency_encoder import DependencyEncoder
-
-#     DEPENDENCIES = {
-#         'advcl': 0, 'advmod': 0, 'obl': 0, 'acl': 1, 'amod': 1, 'nmod': 1, 'nummod': 1,
-#         'compound': 2, 'fixed': 2, 'flat': 2, 'goeswith': 2, 'csubj': 3, 'nsubj': 3,
-#         'appos': 4, 'aux': 5, 'case': 6, 'cc': 7, 'ccomp': 8, 'conj': 9, 'cop': 10,
-#         'dep': 11, 'det': 12, 'iobj': 13, 'list': 14, 'mark': 15, 'obj': 16, 'orphan': 17,
-#         'parataxis': 18, 'vocative': 19, 'xcomp': 20
-#     }
-
-#     we = api.load('glove-wiki-gigaword-50')
-#     nlp = spacy.load('en')
-#     enc = DependencyEncoder(nlp, we, 50, 1, DEPENDENCIES)
-#     layers = [200, 50, 1] # totally random at this point
-#     pred = STSPredictor(enc, layers)
-#     # s1 = "the young boys were playing outside."
-#     # s2 = "the old men were sitting inside."
-#     # l_x, x = pred(s1, s2)
-#     print(pred.encoder.state_dict().keys())
-#     print(pred.parameters())
+        return l_x
