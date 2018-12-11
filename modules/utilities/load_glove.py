8dcbd545bf53b89e95b3434cd748b002a6663784
michael <michael_ball@brown.edu>
2018-12-10 20:47:39 -0500

    added utility for loading glove 840B embeddings

---
+++ modules/utilities/load_glove.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from tqdm import tqdm
+
+
+def load_glove(path):
+    we = {}
+    with Path(path).open('r') as f:
+        for line in tqdm(f, total=2196018):
+            l = line.split(' ')
+            we[l[0]] = np.asarray(l[1:], dtype=np.float32)
+    
+    return we
