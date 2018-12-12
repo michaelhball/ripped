18ecaa26a88a4f7be98e7d74287d753efec171ec
michael <michael_ball@brown.edu>
2018-12-12 01:55:04 -0500

    finished separating all lstm models

--- modules/models/dep_encoder_1_lstm.py
+++ modules/models/dep_encoder_1_lstm.py
@@ -7,7 +7,7 @@ from torch.autograd import Variable
 from .linear_block import LinearBlock
 
 
-class DependencyEncoder3(nn.Module):
+class DEPEncoder1LSTM(nn.Module):
     def __init__(self, embedding_dim, batch_size, dependency_map, evaluate=False):
         """
         Sentence embedding model using dependency parse structure.
@@ -30,27 +30,25 @@ class DependencyEncoder3(nn.Module):
         x = Variable(torch.tensor([node.embedding], dtype=torch.float), requires_grad=True)
         z_cs = []
         for c in node.chidren:
-            dep_index = self.dependency_map[c.dep]
-            D_c = self.params[dep_index]
             x_c = self.recur(c)
-            z_c = D_c(x_c)
-            z_cs.append(z_c)
+            D_c = self.params[self.dependency_map[c.dep]]
+            z_cs.append(D_c(x_c)) # no non-linearity here.
+        
+        zs = torch.stack(z_cs + [x])
+        outputs, hs = self.lstm(zs)
+        z, _ = torch.max(outputs, 0)
+        # z = hs[-1].view(1, -1)
 
-        zs = torch.stack(z_cs + [x]) # nx1xd (n = #children+1)
-        output, hidden_state = self.lstm(zs)
-        # should I make this max pooling here??
-        z = hidden_state[-1].view(1, -1)
-
-        if self.evaluate:
-            node.representation = z.detach().numpy()
-            _, hs = self.lstm(torch.stack([x]))
-            node.embedding = hs[-1].view(1, -1).detach().numpy()
+        # if self.evaluate:
+        #     node.representation = z.detach().numpy()
+        #     _, hs = self.lstm(torch.stack([x]))
+        #     node.embedding = hs[-1].view(1, -1).detach().numpy()
 
         return z
 
     def forward(self, input):
         with torch.set_grad_enabled(self.training):
             output = self.recur(input)
-            if self.evaluate: # return input that's had representations added for every node.
+            if self.evaluate:
                 return input
             return output
--- modules/models/pos_encoder_2_lstm.py
+++ modules/models/pos_encoder_2_lstm.py
@@ -7,8 +7,8 @@ from torch.autograd import Variable
 from .linear_block import LinearBlock
 
 
-class DependencyEncoder3(nn.Module):
-    def __init__(self, embedding_dim, batch_size, dependency_map, evaluate=False):
+class POSEncoder2LSTM(nn.Module):
+    def __init__(self, embedding_dim, batch_size, pos_tags, evaluate=False):
         """
         Sentence embedding model using dependency parse structure.
         Args:
@@ -20,37 +20,30 @@ class DependencyEncoder3(nn.Module):
         super().__init__()
         self.embedding_dim = embedding_dim
         self.batch_size = batch_size
-        self.dependency_map = dependency_map
-        self.num_params = max(list(self.dependency_map.values())) + 1
+        self.pos_tags = pos_tags
+        self.num_params = max(list(self.pos_tags.values())) + 1
         self.params = nn.ModuleList([nn.Linear(self.embedding_dim, self.embedding_dim) for _ in range(self.num_params)])
         self.lstm = nn.LSTM(self.embedding_dim, self.embedding_dim, 1)
         self.evaluate = evaluate
 
     def recur(self, node):
         x = Variable(torch.tensor([node.embedding], dtype=torch.float), requires_grad=True)
-        z_cs = []
-        for c in node.chidren:
-            dep_index = self.dependency_map[c.dep]
-            D_c = self.params[dep_index]
-            x_c = self.recur(c)
-            z_c = D_c(x_c)
-            z_cs.append(z_c)
+        x = self.params[self.pos_tags[node.pos]](x)
+        zs = torch.stack([self.recur(c) for c in node.chidren] + [x])
+        outputs, _ = self.lstm(zs)
+        z, _ = torch.max(outputs, 0)
 
-        zs = torch.stack(z_cs + [x]) # nx1xd (n = #children+1)
-        output, hidden_state = self.lstm(zs)
-        # should I make this max pooling here??
-        z = hidden_state[-1].view(1, -1)
-
-        if self.evaluate:
-            node.representation = z.detach().numpy()
-            _, hs = self.lstm(torch.stack([x]))
-            node.embedding = hs[-1].view(1, -1).detach().numpy()
+        # if self.evaluate:
+        #     node.representation = z.detach().numpy()
+        #     outputs, _ = self.lstm(torch.stack([x]))
+        #     i, _ = torch.max(outputs, 0)
+        #     node.embedding = i.detach().numpy()
 
         return z
 
     def forward(self, input):
         with torch.set_grad_enabled(self.training):
             output = self.recur(input)
-            if self.evaluate: # return input that's had representations added for every node.
+            if self.evaluate:
                 return input
             return output
