771f558144458d95155a5659ea8592fe90183117
michael <michael_ball@brown.edu>
2019-02-21 12:21:29 -0500

    added early stopping capability to training process

--- modules/utilities/__init__.py
+++ modules/utilities/__init__.py
@@ -1,5 +1,6 @@
 from .dependencies import all_dependencies, my_dependencies
-from .helpers import randomise, V
+from .early_stopping import EarlyStopping
+from .helpers import randomise, T, V
 from .load_glove import load_glove
 from .pos_tags import english_tags, universal_tags
 from .preprocessing import convert, create_vocab, tokenise, tokenise_and_embed, tokenise_sent_og, tokenise_sent_tree
