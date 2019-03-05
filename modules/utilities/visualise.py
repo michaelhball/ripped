80d5510676693ceb8292b075786e30bbcbb18914
michael <michael_ball@brown.edu>
2019-03-04 23:48:17 -0500

    removed unused function

--- modules/utilities/visualise.py
+++ modules/utilities/visualise.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 
 
+__all__ = ['plot_train_test_loss']
+
+
 def plot_train_test_loss(train_losses, test_losses, display=True, save_file=None):
     """
     Plots loss through training for train and test datasets
@@ -13,22 +16,6 @@ def plot_train_test_loss(train_losses, test_losses, display=True, save_file=None
     ax.plot(test_losses, 'r-', label='test loss')
     ax.legend()
     ax.set(xlabel='epoch number', ylabel='average loss', title='train/test losses')
-    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    ax.grid()
-    if save_file:
-        plt.savefig(save_file)
-    if display:
-        plt.show()
-
-
-def plot_train_test_accs(train_accs, test_accs, display=True, save_file=None):
-    """
-    Plots accuracy through training for train and test datasets
-    """
-    fig, ax = plt.subplots()
-    ax.plot(train_accs, 'b-', label='train accuracy')
-    ax.plot(test_accs, 'r-', label='test accuracy')
-    ax.set(xlabel='epoch number', ylabel='accuracy', title='train/test accuracies')
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
     ax.grid()
     if save_file:
