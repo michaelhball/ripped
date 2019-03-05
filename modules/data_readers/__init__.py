9e1ebdbe27b4b47f7c0eef4b6a7a5120f111d7db
michael <michael_ball@brown.edu>
2019-03-04 22:13:28 -0500

    data stuff

--- modules/data_readers/__init__.py
+++ modules/data_readers/__init__.py
@@ -1,5 +1,6 @@
 from .intent_classification_data_reader import IntentClassificationDataReader
 from .nli_data_reader import NLIDataReader
 from .senteval_data_reader import SentEvalDataReader
+from .sts_data_reader import STSDataReader
 # from .sick_data_reader import SICKDataReader
 # from .stsbenchmark_data_reader import STSBenchmarkDataReader
