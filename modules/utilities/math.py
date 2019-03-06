a8a3649c37aa3a06b86c025e5010e3c184610d1c
michael <michael_ball@brown.edu>
2019-03-05 22:55:43 -0500

    added more math utilities

--- modules/utilities/math.py
+++ modules/utilities/math.py
@@ -3,17 +3,22 @@ Math utilities.
 """
 import math
 import numpy as np
+import scipy
 
-from scipy import stats
-
-
-__all__ = ['cosine', 'confidence_interval']
+__all__ = ['cosine', 'confidence_interval', 'euclid', 'softmax']
 
 
 def cosine(u, v):
     return np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
 
-def confidence_interval(confidence, std, n):
-    t_value = stats.t.ppf(confidence, n)
-    return t_value * (std / math.sqrt(n))
+def euclid(u,v):
+    return np.linalg.norm(u-v)
+
+
+def softmax(u):
+    return scipy.special.softmax(u)
+
+
+def confidence_interval(p, std, n):
+    return scipy.stats.t.ppf(p, n) * (std / math.sqrt(n))
