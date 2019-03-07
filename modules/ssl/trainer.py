c8192a9af6740ca7edcccd6720025827663a1f9b
michael <michael_ball@brown.edu>
2019-03-07 18:25:03 -0500

    remved grid_search and reorganised some training logic

--- modules/ssl/trainer.py
+++ modules/ssl/trainer.py
@@ -12,54 +12,10 @@ from modules.utilities import repeat_trainer, traina
 from modules.utilities.imports import *
 from modules.utilities.imports_torch import *
 
-from .label_propagation import label_prop_classify, LabelProp
+from .lp import LabelProp
 from .knn import knn_classify
 
-
-__all__ = ['repeat_augment_and_train', 'grid_search_lp', 'self_train']
-
-
-def grid_search_lp(data_source, lp_type, encoder_model, train_ds, text_field, label_field, frac):
-    if lp_type == "propagation":
-        pg = {'kernel': ['knn'], 'gamma': [100], 'n_neighbors': [1], 'alpha': [None], 'max_iter': [1000000], 'tol': [1e-3]} # BEST param set if knn
-        # pg = {'kernel': ['rbf'], 'gamma': [20, 10, 30, 50], 'n_neighbors': [1], 'alpha': [None], 'max_iter': [500, 1000, 2000, 10000, 100000], 'tol': [1e-3, 1e-4]}
-    else:
-        # only need to find the best alpha, but can do this using labeled + unlabeled addition below...
-        pg = {'kernel': ['knn'], 'gamma': [20], 'n_neighbors': [1], 'alpha': [0.1,0.2,0.3,0.4,0.5], 'max_iter': [30], 'tol': [1e-3]}
-
-    results = []
-    param_grid = ParameterGrid(pg)
-    for i, params in tqdm(enumerate(param_grid), total=len(param_grid)):
-        pd = {'kernel': params['kernel'], 'gamma': params['gamma'], 'n_neighbors': params['n_neighbors'],
-                'alpha': params['alpha'], 'max_iter': params['max_iter'], 'tol': params['tol']}
-
-        accs = []
-        for idx in tqdm(range(10), total=10):
-            examples = train_ds.examples
-            np.random.shuffle(examples)
-            cutoff = int(frac*len(examples))
-            labeled_examples = examples[:cutoff]
-            unlabeled_examples = examples[cutoff:]
-
-            embedding_type = encoder_model.split('_')[1]
-            res = encode_data_with_pretrained(data_source, train_ds, text_field, embedding_type, labeled_examples, unlabeled_examples)
-            xs_l, ys_l, xs_u, ys_u, xs_u_unencoded = res
-            # should append xs_u to xs_l, and append labels of -1 for each of these samples (this will be the input to the fit function).
-            # definitely need to try this too.
-
-            xs_l = np.array([x / np.linalg.norm(x) for x in xs_l])
-            xs_u = np.array([x / np.linalg.norm(x) for x in xs_u])
-
-            classifications = label_prop_classify(xs_l, ys_l, xs_u, lp_type, pd)
-            accs.append(np.sum(classifications == ys_u) / len(ys_u))
-        
-        mean, std = np.mean(accs), np.std(accs)
-        # print("-------------------------------------------------------------------------------------------------------------------------------------------")
-        # print(i, mean, std, params)
-        # print("-------------------------------------------------------------------------------------------------------------------------------------------")
-        results.append({'mean': mean, 'std': std, 'params': params})
-    
-    return list(reversed(sorted(results, key=lambda x: x['mean'])))
+__all__ = ['repeat_augment_and_train', 'self_feed']
 
 
 def self_feed(dir_to_save, labeled_examples, unlabeled_examples, val_ds, test_ds, text_field, label_field, classifier_params):
@@ -81,12 +37,12 @@ def self_feed(dir_to_save, labeled_examples, unlabeled_examples, val_ds, test_ds
     
     while True:
         new_train_ds = data.Dataset(labeled_examples, {'x': text_field, 'y': label_field})
-        wrapper = train_ic(dir_to_save, text_field, label_field, (new_train_ds, val_ds, test_ds), classifier_params)
+        wrapper = train_ic(dir_to_save, text_field, label_field, (new_train_ds, val_ds, test_ds), classifier_params, return_statistics=False)
         confidences, preds = wrapper.classify_all(unlabeled_examples)
 
         new_unlabeled_examples = []
         for i, (conf, pred) in enumerate(zip(confidences, preds)):
-            if conf > 0.98: # PLAY AROUND WITH THIS THRESHOLD
+            if conf > 0.99:
                 labeled_examples.append(Example.fromdict({'x': unlabeled_examples[i].x, 'y': pred}, {'x': ('x', text_field), 'y': ('y', label_field)}))
                 num_added += 1
                 if pred == unlabeled_examples[i].y:
@@ -120,9 +76,9 @@ def repeat_augment_and_train(dir_to_save, data_source, aug_algo, encoder_model, 
     """
     train_ds, val_ds, test_ds = datasets
     aug_accs, aug_fracs = [], []
-    means, stds = [], []
+    class_accs = []
     ps, rs, fs = [], [], []
-    for i in range(k):
+    for i in tqdm(range(k),total=k):
         examples = train_ds.examples
         np.random.shuffle(examples)
         cutoff = int(frac*len(examples))
@@ -132,30 +88,30 @@ def repeat_augment_and_train(dir_to_save, data_source, aug_algo, encoder_model, 
         if aug_algo == "self_feed" and frac < 1:
             augmented_train_examples, aug_acc, frac_used = self_feed(dir_to_save, labeled_examples, unlabeled_examples, val_ds, test_ds, text_field, label_field, classifier_params)
             aug_accs.append(aug_acc); aug_fracs.append(frac_used)
-        elif aug_algo and frac < 1:
+        elif aug_algo != "none" and frac < 1:
             augmented_train_examples, aug_acc, frac_used = augment(data_source, aug_algo, encoder_model, labeled_examples, unlabeled_examples, train_ds, text_field, label_field)
             aug_accs.append(aug_acc); aug_fracs.append(frac_used)
         else:
             augmented_train_examples = labeled_examples
-            aug_accs.append(1)
+            aug_accs.append(1); aug_fracs.append(0)
 
         new_train_ds = data.Dataset(augmented_train_examples, {'x': text_field, 'y': label_field})
         new_datasets = (new_train_ds, val_ds, test_ds)
-        mean, std, avg_p, avg_r, avg_f = repeat_ic(dir_to_save, text_field, label_field, new_datasets, classifier_params)
-        means.append(mean); stds.append(std); ps.append(avg_p); rs.append(avg_r); fs.append(avg_f)
+        acc, p, r, f = train_ic(dir_to_save, text_field, label_field, new_datasets, classifier_params, return_statistics=True)
+        class_accs.append(acc); ps.append(p); rs.append(r); fs.append(f)
 
     print(f"FRAC '{frac}' RESULTS BELOW:")
-    print(f'augmentation acc mean: {np.mean(aug_accs)}, augmentation acc std: {np.std(aug_accs)}, augmentation frac mean: {np.mean(aug_fracs)}')
-    print(f'precision mean: {np.mean(ps)}, recall mean: {np.mean(rs)}, f1 mean: {np.mean(fs)}')
-    print(f'class acc mean: {np.mean(means)}, class acc std: {np.std(means)}, mean of stds: {np.mean(stds)}')
+    print(f'classification acc --> mean: {np.mean(class_accs)}, std: {np.std(class_accs)}')
+    print(f'augmentation acc --> mean: {np.mean(aug_accs)}, std: {np.std(aug_accs)}\t (average frac used: {np.mean(aug_fracs)})')
+    print(f'p/r/f1 --> precision mean: {np.mean(ps)}; recall mean: {np.mean(rs)}; f1 mean: {np.mean(fs)}')
+    print(f'p/r/f1 --> precision std: {np.std(ps)}; recall std: {np.std(rs)}; f1 std: {np.std(fs)}')
 
-    class_acc_mean, class_acc_std = np.mean(means), np.std(means)
-    aug_acc_mean, aug_acc_std = np.mean(aug_accs), np.std(aug_accs)
-    aug_frac_mean = np.mean(aug_fracs)
-    p_mean, r_mean, f1_mean = np.mean(ps), np.mean(rs), np.mean(fs)
-    p_std, r_std, f1_std = np.std(ps), np.std(rs), np.std(fs)
+    class_acc_mean, class_acc_std = np.mean(class_accs), np.std(class_accs)
+    aug_acc_mean, aug_acc_std, aug_frac_mean = np.mean(aug_accs), np.std(aug_accs), np.mean(aug_fracs)
+    p_mean, r_mean, f_mean = np.mean(ps), np.mean(rs), np.mean(fs)
+    p_std, r_std, f_std = np.std(ps), np.std(rs), np.std(fs)
 
-    return class_acc_mean, class_acc_std, aug_acc_mean, aug_acc_std, p_mean, p_std, r_mean, r_std, f1_mean, f1_std, aug_frac_mean
+    return class_acc_mean, class_acc_std, aug_acc_mean, aug_acc_std, aug_frac_mean, p_mean, p_std, r_mean, r_std, f_mean, f_std
 
 
 def augment(data_source, aug_algo, encoder_model, labeled_examples, unlabeled_examples, train_ds, text_field, label_field, normalise_encodings=True):
@@ -170,29 +126,47 @@ def augment(data_source, aug_algo, encoder_model, labeled_examples, unlabeled_ex
         xs_l = np.array([x / np.linalg.norm(x) for x in xs_l])
         xs_u = np.array([x / np.linalg.norm(x) for x in xs_u])
     
-    if aug_algo == "knn":
-        classifications, num_correct = knn_classify(5, xs_l, ys_l, xs_u, ys_u, weights='uniform', distance_metric='euclidean')
-    elif aug_algo == "my_label_prop":
-        # perhaps I need to recur here and do the LP again (assuming correctness of new labels) if there's a sufficient number of unused unlabeled examples.
-        lp = LabelProp(xs_l, ys_l, xs_u, 21) # hardcoded number of classes
-        lp.propagate()
-        classifications, indices = lp.classify(ys_u,threshold=True)
-        ys_u = ys_u[indices]
-        xs_u_unencoded = [xs_u_unencoded[idx] for idx in indices]
-    elif aug_algo.startswith("label_prop"):
-        type_ = aug_algo.split('__')[1]
-        pd = {'kernel': 'knn', 'gamma': 20, 'n_neighbors': 1, 'alpha': None, 'max_iter': 100000, 'tol': 1e-3}
-        classifications = label_prop_classify(xs_l, ys_l, xs_u, type_, pd)
+    if aug_algo.startswith("knn"):
+        algo_version = aug_algo.split('_')[1]
+        if algo_version == 'base':
+            classifications, indices = knn_classify(1, xs_l, ys_l, xs_u, weights='uniform', distance_metric='euclidean')
+            frac_used = 1
+        elif algo_version == 'threshold':
+            classifications, indices = knn_classify(5, xs_l, ys_l, xs_u, weights='uniform', distance_metric='euclidean', threshold=0.99)
+            ys_u = ys_u[indices]
+            xs_u_unencoded = [xs_u_unencoded[idx] for idx in indices]
+            frac_used = float(len(xs_u_unencoded)/len(xs_u))
+        elif algo_version == "recursive":
+            pass
+    elif aug_algo.startswith("lp"):
+        algo_version = aug_algo.split('_')[1]
+        if algo_version == 'base':
+            pass
+        elif algo_version == "threshold":
+            lp = LabelProp(xs_l, ys_l, xs_u, 21) # hardcoded number of classes
+            lp.propagate()
+            classifications, indices = lp.classify(threshold=True)
+            ys_u = ys_u[indices]
+            xs_u_unencoded = [xs_u_unencoded[idx] for idx in indices]
+            frac_used = float(len(xs_u_unencoded)/len(xs_u))
+        elif algo_version == "recursive":
+            lp = LabelProp(xs_l, ys_l, xs_u, 21) # hardcoded number of classes
+            classifications, indices = lp.recursive(xs_l, ys_l, xs_u, ys_u)
+            ys_u = ys_u[indices]
+            xs_u_unencoded = [xs_u_unencoded[idx] for idx in indices]
+            frac_used = float(len(xs_u_unencoded)/len(xs_u))
+        elif algo_version == "p1nn":
+            lp = LabelProp(xs_l, ys_l, xs_u, 21)
+            classifications, indices = lp.p1nn(xs_l, ys_l, xs_u)
+            frac_used = 1
     
     num_correct = np.sum(classifications == ys_u)
-    frac_used = float(len(xs_u_unencoded)/len(xs_u))
-    
-    # unlabeled = [[text_field.vocab.itos[idx] for idx in sent] for sent in xs_u_unencoded] -- only need to do this if we idxs not strings
-    new_labeled_data = [{'x': x, 'y': classifications[i]} for i,x in enumerate(xs_u_unencoded)]
+        
+    new_labeled_data = [{'x': x, 'y': classifications[i]} for i, x in enumerate(xs_u_unencoded)]
     example_fields = {'x': ('x', text_field), 'y': ('y', label_field)}
     new_examples = [Example.fromdict(x, example_fields) for x in new_labeled_data]
 
-    return labeled_examples + new_examples, float(num_correct / len(classifications)), frac_used
+    return labeled_examples + new_examples, float(num_correct/len(classifications)), frac_used
 
 
 def encode_data_with_pretrained(data_source, train_ds, text_field, embedding_type, examples_l, examples_u):
@@ -234,7 +208,7 @@ def create_pretrained_embeddings(train_ds, text_field, data_source, embedding_ty
     pickle.dump(embeddings, Path('./data/ic/{data_source}/{embedding_type}_embeddings.pkl').open('wb'))
 
 
-def train_ic(dir_to_save, text_field, label_field, datasets, classifier_params):
+def train_ic(dir_to_save, text_field, label_field, datasets, classifier_params, return_statistics=False):
     """
     To simplify training (connection to generic training utility with appropriate parameters).
     # ULTIMATELY, classifier_params should just be able to be passed to this train method along with other
@@ -244,6 +218,10 @@ def train_ic(dir_to_save, text_field, label_field, datasets, classifier_params):
     wrapper = traina(ps['model_name'], ps['encoder_model'], get_ic_data_iterators, IntentWrapper, dir_to_save,
                     nn.CrossEntropyLoss(), datasets, text_field, ps['bs'], ps['encoder_args'],
                     ps['layers'], ps['drops'], ps['lr'], frac=1, verbose=False)
+    if return_statistics:
+        acc = wrapper.test_accuracy(wrapper.test_di)
+        p, r, f, _ = wrapper.test_precision_recall_f1()
+        return acc, np.mean(p), np.mean(r), np.mean(f)
     return wrapper
 
 
