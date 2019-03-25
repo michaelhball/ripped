bab91e281fdc4ef42241af7a3995b268fd6b8aab
michael <michael_ball@brown.edu>
2019-03-25 18:57:25 -0400

    more default imports

--- modules/utilities/imports.py
+++ modules/utilities/imports.py
@@ -1,7 +1,10 @@
 import json
 import math
 import numpy as np
+import pandas as pd
 import pickle
+import random
+import re
 import time
 
 from pathlib import Path
--- modules/utilities/imports_torch.py
+++ modules/utilities/imports_torch.py
@@ -3,3 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from torch.autograd import Variable
+
+from torchtext import data, vocab
+from torchtext.data.example import Example
