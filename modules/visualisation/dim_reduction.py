a31ee93a2fe4c07e78e429ff48689d76b3cc7038
michael <michael_ball@brown.edu>
2019-04-03 13:46:46 -0400

    allowing for calculation of I value

--- modules/visualisation/dim_reduction.py
+++ modules/visualisation/dim_reduction.py
@@ -17,8 +17,25 @@ def visualise_data(data_source, encoder_model, datasets, intents, text_field, ty
     elif encoder_model.startswith('sts'):
         embedding_type = encoder_model
 
-    X, Y, *_ = encode_data_with_pretrained(data_source, train_ds, text_field, embedding_type,  train_ds.examples, [])
-    X = np.array([x / np.linalg.norm(x) for x in X])
+    X, Y, *_ = encode_data_with_pretrained(data_source, train_ds, test_ds, text_field, embedding_type, train_ds.examples + test_ds.examples, [])
+
+    nc = len(set(Y))
+    intent_embeddings = {i: [] for i in range(nc)}
+    for x,y in zip(X,Y):
+        intent_embeddings[y].append(x)
+    intent_stats = {}
+    for intent, embeddings in intent_embeddings.items():
+        intent_stats[intent] = (np.mean(embeddings, axis=0), np.var(embeddings, axis=0))
+    
+    cluster_distances = []
+    for i1, stats in intent_stats.items():
+        max_dist = 0
+        for i2, other_stats in intent_stats.items():
+            dist = np.linalg.norm(np.abs(stats[0]-other_stats[0]), axis=0)
+            if dist > max_dist:
+                max_dist = dist
+        cluster_distances.append(np.linalg.norm(stats[1]) / max_dist)
+    print(f'{np.mean(cluster_distances)}\t{np.max(cluster_distances)}\t{np.min(cluster_distances)}\t{np.mean([s[1] for s in intent_stats.values()])}')
 
     if type_ == "pca":
         pca = PCA(n_components=3)
@@ -37,9 +54,10 @@ def visualise_data(data_source, encoder_model, datasets, intents, text_field, ty
         n_components = {'webapps': 30, 'chatbot': 50, 'askubuntu': 50, 'chat': 100}[data_source]
         pca = PCA(n_components=n_components)
         pca_res = pca.fit_transform(X)
-        print(f'Cumulative explained variation for {n_components} principal components: {np.sum(pca.explained_variance_ratio_)}')
+        # print(f'Cumulative explained variation for {n_components} principal components: {np.sum(pca.explained_variance_ratio_)}')
         tsne = TSNE(n_components=2, verbose=0, perplexity=30, n_iter=1000)
         tsne_pca_res = tsne.fit_transform(pca_res)
+        # tsne_pca_res = tsne.fit_transform(X)
         ts1, ts2 = tsne_pca_res[:,0], tsne_pca_res[:,1]
         df_tsne_pca = DataFrame([{
             'intent': intents[y],
@@ -58,18 +76,8 @@ def visualise_data(data_source, encoder_model, datasets, intents, text_field, ty
                 values = [v[1:] for v in df_tsne_pca.loc[df_tsne_pca['intent']==intent].drop(columns=['intent']).values]
                 mean = np.mean(values, axis=0); var = np.var(values, axis=0)
                 intent_stats[intent] = (mean, np.var(values))
-                ax.scatter([v[0] for v in values], [v[1] for v in values], label=intent, color=f'C{idx}')
-                ax.add_patch(Ellipse(mean, np.sqrt(var[0]), np.sqrt(var[1]), fill=False, edgecolor=f'C{idx}')) # sqrt here makes variance-->std
-            
-            cluster_distances = [] # not really 'distance'
-            for _, stats in intent_stats.items():
-                max_dist = 0
-                for _, other_stats in intent_stats.items():
-                    dist = np.linalg.norm(np.abs(stats[0]-other_stats[0]))
-                    if dist > max_dist:
-                        max_dist = dist
-                cluster_distances.append(stats[1] / max_dist)
-            print(f'avg cluster dist: {np.mean(cluster_distances)}, max cluster dist: {np.max(cluster_distances)}')
+                ax.scatter([v[0] for v in values], [v[1] for v in values], label=intent, color=f'C{idx}', s=100)
+                ax.add_patch(Ellipse(mean, np.sqrt(var[0]), np.sqrt(var[1]), fill=False, edgecolor=f'C{idx}', linewidth=2)) # sqrt here makes variance-->std
             
             if show:
                 plt.xlabel('tsne-pca-1')
